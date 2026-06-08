a6cd72828e9c0ac8b41437a54321eb2a468a1e68
Harrison Reid <H.Reid17@outlook.com>
2026-06-08 13:24:12 +0100

    Create initial application routes and navigation layout

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Dashboard
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
+
+
+# Applications
+@app.route("/applications")
+def applications():
+    return render_template("applications.html")
+
+
+# Analytics
+@app.route("/analytics")
+def analytics():
+    return render_template("analytics.html")
 
 
 if __name__ == "__main__":
